f3032423535a822bf732c44fde23c3a245c5d5a8
OLGA BRAIKO <Olga.braiko456@mail.ru>
2023-08-03 23:17:21 -0700

    this day

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Header from "./components/Header";
+import ThisDay from "./components/ThisDay";
 import "./App.css";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <>
       <div className="container">
         <Header />
+        <ThisDay />
       </div>
     </>
   );
