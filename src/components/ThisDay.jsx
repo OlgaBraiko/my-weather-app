3283a28c99068d770fc057b0b238309b53e51b87
OLGA BRAIKO <Olga.braiko456@mail.ru>
2023-08-04 15:32:53 -0700

    commit

---
+++ src/components/ThisDay.jsx
@@ -0,0 +1,30 @@
+import styles from "./ThisDay.module.css";
+import Image from "../../public/Image";
+
+function ThisDay() {
+  return (
+    <>
+      <header className={styles.today}>
+        <div className={styles.top__block}>
+          <div className={styles.top__block_wrapper}>
+            <div className={styles.this__temp}>20</div>
+            <div className={styles.this__day}>Сегодня</div>
+          </div>
+          <Image id="sun" />
+        </div>
+
+        <div className={styles.bottom__block}>
+          <div className={styles.this__time}>
+            Время: <span>21:54</span>
+          </div>
+          <div className={styles.this__city}>
+            Город: <span>Санкт-Петербург</span>
+          </div>
+        </div>
+      </header>
+
+      <div className="wrapper"></div>
+    </>
+  );
+}
+export default ThisDay;
